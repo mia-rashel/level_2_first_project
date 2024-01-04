b6ed0e86fe732172a7988982d0f46ff2e4c7ff50
Muhammad Rashel Mia <muhammadrashel2015@gmail.com>
2024-01-04 12:25:42 -0500

    first commit

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+const port = 3000
+
+//parser
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10
+
+  res.send(a)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+export default app
